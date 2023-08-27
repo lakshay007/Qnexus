bf17caa2b84f35337cede57873ee8f7426bc171c
Lakshay <34618792+lakshay007@users.noreply.github.com>
2023-08-27 11:50:56 +0530

    Add files via upload

---
+++ src/firebase1/firebaseConfig.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7IJYAOLlSu9__gxX8RZZmBS9zB1vjooE",
+  authDomain: "svelteproj-b80fb.firebaseapp.com",
+  projectId: "svelteproj-b80fb",
+  storageBucket: "svelteproj-b80fb.appspot.com",
+  messagingSenderId: "56478471242",
+  appId: "1:56478471242:web:aa6cf0d822a804e8b4c2b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { getAuth, signOut } from "firebase/auth";
+    import {auth} from "../../firebase1/firebaseConfig";
+const handlesignout = () =>{
+
+signOut(auth).then(() => {
+  window.location.href = "/";
+}).catch((error) => {
+  // An error happened.
+});
+}
+</script>
+<main>
+<a href="#" on:click|preventDefault={handlesignout}>logout</a>
+</main>
+
+<style>
+
+</style>
