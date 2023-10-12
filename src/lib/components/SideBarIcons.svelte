1571a65c0acfdb7002b5eee45eb9ac19625ddc18
Lepnoxic <akashdevulapalli@gmail.com>
2023-10-12 22:34:34 +0530

    additions

---
+++ src/lib/components/SideBarIcons.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Icon from 'svelte-icons-pack/Icon.svelte';
+    import BsPlus  from 'svelte-icons-pack/bs/BsPlus';
+    export let styleid = BsPlus;
+    export let text = 'tooltip 💡';
+</script>
+<div class="relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-gray-800 text-blue-400 hover:bg-blue-500 hover:text-white rounded-3xl hover:rounded-xl transition-all duration-100 ease-linear cursor-pointer group">
+    <Icon src={styleid}>
+    </Icon>
+
+    <span class="absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md bg-gray-900 text-white text-xs font-bold transition-all duration-0 scale-0 origin-left group-hover:scale-100 transition-all">
+        {text}
+    </span>
+</div>
---
+++ src/lib/components/bars.svelte
@@ -0,0 +1,60 @@
+<script>
+    import { getAuth, signOut } from "firebase/auth";
+    import {auth} from "../../firebase1/firebaseConfig";
+    
+    import logo from "../../assets/QNexus_Black-removebg-preview.png";
+    import SideBarIcons from '$lib/components/SideBarIcons.svelte';
+    import BsBook from 'svelte-icons-pack/bs/BsBookFill';
+    import BsGearFill  from 'svelte-icons-pack/bs/BsClipboard2CheckFill';
+    import BsJoystick from 'svelte-icons-pack/bs/BsJoystick';
+    import BsHouseFill from 'svelte-icons-pack/bs/BsHouseFill';
+    import BsBar from 'svelte-icons-pack/bs/BsBarChartFill';
+    const handlesignout = () =>{ //function for signout
+        signOut(auth).then(() => {     
+            window.location.href = "/";
+        }).catch((error) => {
+            console.log("Error during signout.");
+        });
+    }
+</script>
+<style>
+    #main{
+        overflow: hidden;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+    #navbar1{
+        flex-shrink: 1;
+    }
+    #sidebar{
+        flex-grow: 1;
+    }
+</style>
+<main class="select-none">
+    <div id="main" class="h-screen bg-transparent">
+        <div id="navbar1" class="bg-white flex flex-row items-center p-3 shadow-2xl">
+            <div class="flex-1">
+                <div class="flex flex-row mx-5 group gap-4 ">
+                    <img src={logo} alt="qnexus logo" class="" style="height: 36px"/>
+                    <p class="normal-case font-semibold text-3xl text-black" style="font-family: Merriweather">QNexus</p>
+                </div>
+            </div>
+            <div class="flex flex-row gap-4 mx-5 text-black" style="font-family: Merriweather">
+                <div class="btn btn-ghost"><a class="normal-case text-2xl font-semibold" href="/profile">Profile</a></div>
+                <div class="btn btn-ghost"><a class="normal-case text-2xl font-semibold" on:click={handlesignout}>Sign Out</a></div>
+            </div>
+        </div>
+        <div id="sidebar" class="bg-transparent flex flex-row">
+            <div class=" w-24 m-0 flex flex-col gap-y-3 shadow-lg bg-inherit text-white">
+                <div class="h-3"></div>
+                <a href="/dashboard"><SideBarIcons styleid={BsHouseFill} text="Home"></SideBarIcons></a>
+                <SideBarIcons styleid={BsBook} text="Learn"></SideBarIcons>
+                <SideBarIcons styleid={BsJoystick} text="Compete"></SideBarIcons>
+                <SideBarIcons styleid={BsGearFill} text="Goals"></SideBarIcons>
+                <SideBarIcons styleid={BsBar} text="LeaderBoard"></SideBarIcons>
+            </div>
+            <slot />
+        </div>
+    </div>
+</main>
