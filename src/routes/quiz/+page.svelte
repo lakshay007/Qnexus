a6d8e913240e1da8a6a3a8253365d7af49fe5cf1
Lepnoxic <akashdevulapalli@gmail.com>
2023-10-19 14:51:55 +0530

    quiz alpha

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -19,21 +19,37 @@ const click = async() =>{
     window.location.href = "play";  
 }
 </script>
-<main>
-    <button on:click={click} >
-        play
-    </button>
-    <a href="/join" >
-        join
-    </a>
-    <a href="/contests" >
-        contest1
-    </a>
-    <a href="/contests" >
-        contest2
-    </a>
-</main>
-
 <style>
-
+    #challenges {
+        background-size: 35% auto;
+        background: url("../../assets/challenges.jpeg"), linear-gradient(#000000 100%, #000000 100%);
+        background-repeat: no-repeat;
+        background-position: center left;
+    }
 </style>
+<main class="flex flex-col grow items-center">
+    <div id="challenges" class=" flex flex-row h-[12.222vh] my-[4vh] w-[72vw] justify-end items-center">
+        <div class="font-bold text-3xl text-white mr-5">
+            CHALLENGES
+        </div>
+    </div>
+    <div id="onevsone" class="bg-green-400 w-[67.5vw] h-[20vh] mb-[4vh]">
+        <button on:click={click} >
+            play
+        </button>
+        <a href="/join" >
+            join
+        </a>
+    </div>
+    <div id="upcoming" class="bg-red-400 w-[67.5vw] mb-[3.25vh]">
+        upcoming
+    </div>
+    <div id="contests" class="grow bg-yellow-700 w-[67.5vw] mb-[3.25vh]">
+        <a href="/contests" >
+            contest1
+        </a>
+        <a href="/contests" >
+            contest2
+        </a>
+    </div>
+</main>
