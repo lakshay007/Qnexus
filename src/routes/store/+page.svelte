b96be6889da8dc4f1a349c3c0304790b74887bd9
Lepnoxic <akashdevulapalli@gmail.com>
2023-10-20 02:41:23 +0530

    store beta

--- src/routes/store/+page.svelte
+++ src/routes/store/+page.svelte
@@ -1,26 +1,31 @@
 <script>
-    let coins = 1;
-    let current = 0;
+    let coins = 1000;
+    let current = 200;
     import money from "../../assets/money.png";
     import coin from "../../assets/coin.png"
+    import StorePart from "../../lib/components/storePart.svelte";
+    import coupon from "../../assets/coupon.jpg";
 </script>
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Merriweather&display=swap');
 </style>
-<main class="grow bg-slate-500 flex flex-row">
-    <div class="bg-blue-500 grow flex flex-col justify-center items-center gap-y-[1vh]">
+<main class="grow flex flex-row items-center justify-evenly">
+    <div class=" flex flex-col justify-center items-center gap-y-[1vh]">
         <div class="font-['MerriWeather'] font-bold text-center text-white">
             V A U L T
         </div>
-        <div class="flex flex-row items-center bg-zinc-700">
+        <div class="flex flex-row items-center ">
             <img src={coin} class="h-auto w-[5vw]" alt=""/>
-            <p class="text-[3.333vw] font-['MerriWeather']">{current}/{coins}</p>
+            <p class="text-[3.333vw] font-['MerriWeather'] text-white">{current}/{coins}</p>
         </div>
-        <div>
-            <img src={money} alt=""/>
+        <div class="">
+            <img src={money} class="w-[16vw] h-auto" alt=""/>
         </div>
     </div>
-    <div class="bg-green-400 grow">
-        ba
+    <div class="flex flex-row flex-wrap m-[5vw] gap-[2vw]">
+        <StorePart image={coupon} text="Coupon at max" price="100"></StorePart>
+        <StorePart image={coupon} text="Coupon at max" price="100"></StorePart>
+        <StorePart image={coupon} text="Coupon at max" price="100"></StorePart>
+        <StorePart image={coupon} text="Coupon at max" price="100"></StorePart>
     </div>
 </main>
