4f372130211edb83069dbe78f7d197e6827aadec
Lepnoxic <akashdevulapalli@gmail.com>
2023-10-20 15:18:06 +0530

    course mat update

--- src/routes/coursemat/+page.svelte
+++ src/routes/coursemat/+page.svelte
@@ -15,7 +15,7 @@
     sub = 1;
   };
 </script>
-<main>
+<main class="">
     {#if sub == 0}
 <h1>your subjects</h1>
     <a href="#" on:click={() => onclick("Chemistry")}>chemistry <br></a>
@@ -23,7 +23,9 @@
     <a href="#" on:click={() => onclick("Physics")}>physics <br></a>
     {/if}
     {#if chem === 1}
-    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSfKNxC250AWjH2RSp4d_ZJ0zG0Aphm_VwIw0dJG9hMx59KCfcwKa9B90lnSjxzjmLMmtoexx5eIeIo/embed?start=false&loop=false&delayms=60000" frameborder="0" width="1500" height="910" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" title=""></iframe>
+    <div class="grow flex flex-row justify-center overflow-hidden h-screen w-screen m-0 absolute items-center bg-black">
+      <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSfKNxC250AWjH2RSp4d_ZJ0zG0Aphm_VwIw0dJG9hMx59KCfcwKa9B90lnSjxzjmLMmtoexx5eIeIo/embed?start=false&loop=false&delayms=60000&widget=false&headers=false&chrome=false" frameborder="0" width="1500" height="910" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" title="" class="absolute justify-self-center w-[85vw]"></iframe>
+    </div>
     {/if}
 </main>
 <style>
