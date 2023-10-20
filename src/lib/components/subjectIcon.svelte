78c4a800a3307f0a7d5d721bc2a3bea423a3c3fa
Lepnoxic <akashdevulapalli@gmail.com>
2023-10-20 17:57:03 +0530

    coursemat beta

---
+++ src/lib/components/subjectIcon.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let ref="/";
+    export let title="";
+    export let src="";
+</script>
+
+<a href={ref} class="flex flex-col justify-start items-center gap-y-9">
+    <img src={src} class="rounded-full h-[200px]" alt=""/>
+    <p class="text-white font-['MerriWeather'] text-2xl capitalize">{title}</p>
+</a>
--- src/routes/coursemat/+page.svelte
+++ src/routes/coursemat/+page.svelte
@@ -5,21 +5,24 @@
     import SubjectIcon from "../../lib/components/subjectIcon.svelte";
 </script>
 <style>
-
+  @import url('https://fonts.googleapis.com/css2?family=Acme&family=DM+Sans:opsz@9..40&family=Exo&family=K2D&family=Kaisei+Tokumin&family=Karma:wght@500&family=KoHo:wght@500&family=Merriweather&family=Offside&family=Ruslan+Display&display=swap');
+    #material {
+        background: url("../../assets/material.png"), linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1));
+        background-size: auto 100%;
+        background-repeat: no-repeat;
+        background-position: center left;
+        font-family: "Acme";
+    }
 </style>
-<main class="flex flex-col grow items-center">
+<main class="flex flex-col grow items-center gap-y-8">
   <div id="material" class=" flex flex-row h-[12.222vh] my-[2vh] w-[72vw] justify-center items-center">
       <div class="font-medium text-4xl text-grey-800 text-center">
           STUDY MATERIAL
       </div>
-      <div>
-
-      </div>
   </div>
-  <div class="grow flex flex-col gap-y-[4vh]">
-    <SubjectIcon></SubjectIcon>
-    <a href="/chemcourse">chemistry</a>
-    <a href="/mlmat">Ml</a>
-    <a href="/phycourse">physics</a>
+  <div class="grow flex flex-row gap-x-[4vh] m-[4vh]">
+    <SubjectIcon ref="/chemcourse" title="chemistry" src={chem}/>
+    <SubjectIcon ref="/mlmat" title="ML - I" src={ml}/>
+    <SubjectIcon ref="/phycourse" title="physics" src={physics}/>
   </div>
 </main>
