8cb0cc8a4402f2cbb6a4b0dd983eaa6c938c1b55
Lakshay <dmslakshay@gmail.com>
2023-10-20 18:03:32 +0530

    mlmat added

--- src/routes/mlmat/+page.svelte
+++ src/routes/mlmat/+page.svelte
@@ -1,3 +1,5 @@
 <main>
-    
+    <div class="grow flex flex-row justify-center overflow-hidden h-screen w-screen m-0 absolute items-center bg-black">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRnT902ef8Rw3t9W2vZ2B7oSvi9Bvs_I8J-goCcUZ4QG9_IKeeaTl68RejPczXZaq1JbTacf6xWLL1E/embed?start=false&loop=false&delayms=60000" frameborder="0" width="1800" height="1800" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+</div>
 </main>
