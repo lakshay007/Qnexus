6d16e8f4236656442a3cc8528d8954cdfeecf34d
Lepnoxic <akashdevulapalli@gmail.com>
2023-10-20 17:42:23 +0530

    coursemat alpha

--- src/routes/coursemat/+page.svelte
+++ src/routes/coursemat/+page.svelte
@@ -1,28 +1,25 @@
 <script>
- let val = "",sub= 0;
-  let chem = 0,ml = 0,phy = 0;
-  let onclick = (subject) => {
-    val = subject;
-    if (val === "Chemistry") {
-        window.location.href = "chemcourse";
-    }
-    else if(val === "Ml"){
-        window.location.href = "mlmat";
-    }
-    else if(val === "Physics"){
-        window.location.href = "phycourse";
-    }
-    sub = 1;
-  };
+  import physics from "../../assets/physics.jpeg";
+  import chem from "../../assets/chem.jpeg";
+  import ml from "../../assets/ML.jpeg";
+    import SubjectIcon from "../../lib/components/subjectIcon.svelte";
 </script>
-<main class="">
-    {#if sub == 0}
-<h1>your subjects</h1>
-    <a href="#" on:click={() => onclick("Chemistry")}>chemistry <br></a>
-    <a href="#" on:click={() => onclick("Ml")}>Ml <br></a>
-    <a href="#" on:click={() => onclick("Physics")}>physics <br></a>
-    {/if}
-</main>
 <style>
 
 </style>
+<main class="flex flex-col grow items-center">
+  <div id="material" class=" flex flex-row h-[12.222vh] my-[2vh] w-[72vw] justify-center items-center">
+      <div class="font-medium text-4xl text-grey-800 text-center">
+          STUDY MATERIAL
+      </div>
+      <div>
+
+      </div>
+  </div>
+  <div class="grow flex flex-col gap-y-[4vh]">
+    <SubjectIcon></SubjectIcon>
+    <a href="/chemcourse">chemistry</a>
+    <a href="/mlmat">Ml</a>
+    <a href="/phycourse">physics</a>
+  </div>
+</main>
