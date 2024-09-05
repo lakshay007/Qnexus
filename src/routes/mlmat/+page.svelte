c203d0135024e41d91ff5612d04de32e4adcaae7
Lakshay <dmslakshay@gmail.com>
2024-09-05 16:52:39 +0530

    slides update

--- src/routes/mlmat/+page.svelte
+++ src/routes/mlmat/+page.svelte
@@ -1,6 +1,6 @@
 <main>
     <div class="grow flex flex-row justify-center overflow-hidden h-screen w-screen m-0 absolute items-center bg-black">
-    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRnT902ef8Rw3t9W2vZ2B7oSvi9Bvs_I8J-goCcUZ4QG9_IKeeaTl68RejPczXZaq1JbTacf6xWLL1E/embed?start=false&loop=false&delayms=60000" frameborder="0" width="1800" height="1800" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" title=""></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRnT902ef8Rw3t9W2vZ2B7oSvi9Bvs_I8J-goCcUZ4QG9_IKeeaTl68RejPczXZaq1JbTacf6xWLL1E/embed?start=false&loop=false&delayms=60000" frameborder="0" width="1800" height="1800" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
     <a href="/dashboard">
         <button type="button" class="btn btn-circle btn-outline absolute bottom-[5vh] left-[3vw]">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
