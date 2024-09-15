62a144550e68977001f0b9bdbc24093465e3dad5
Lakshay <dmslakshay@gmail.com>
2024-09-15 16:34:02 +0530

    mobile responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,6 +34,7 @@
   @import url('https://fonts.googleapis.com/css2?family=Acme&family=DM+Sans:opsz@9..40&family=K2D&family=Kaisei+Tokumin&family=Merriweather&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Acme&family=DM+Sans:opsz@9..40&family=K2D&family=Kaisei+Tokumin&family=KoHo:wght@500&family=Merriweather&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Acme&family=DM+Sans:opsz@9..40&family=K2D&family=Kaisei+Tokumin&family=Karma:wght@500&family=KoHo:wght@500&family=Merriweather&display=swap');
+
   #ani2 {
     opacity: 0;
     filter: blur(2.5px);
@@ -63,83 +64,131 @@
     height: 100vh;
   }
   .q-n-e-x-u-s {
-  color: #ffffff;
-  text-align: left;
-  font: 600 5vw "K2D", sans-serif;
-  position: absolute;
-  left: 3.45vw;
-  top: 130vh;
-  text-shadow: 0 0 7px black;
-}
-.an-alternate-reality-an-alternate-vision {
-  color: #ffffff;
-  text-align: left;
-  font: 600 1.667vw "K2D", sans-serif;
-  position: absolute;
-  left: 3.45vw;
-  top: 145vh;
-  text-shadow: 0 0 7px black;
-}
-.learn {
-  color: #d0f4ff;
-  text-align: left;
-  font: 600 4.1667vw "KoHo", sans-serif;
-  position: absolute;
-  left: 60.677vw;
-  top: 122.5vh;
-}
-.compete {
-  color: #d0f4ff;
-  text-align: left;
-  font: 600 4.1667vw "KoHo", sans-serif;
-  position: absolute;
-  left: 77.2916vw;
-  top: 143.88vh;
-}
-.climb {
-  color: #d0f4ff;
-  text-align: left;
-  font: 600 4.1667vw "KoHo", sans-serif;
-  position: absolute;
-  left: 69.0625vw;
-  top: 167.5vh;
-}
-.browse-extensive-course-material {
-  color: #ffffff;
-  text-align: left;
-  font: 600 2.0833vw "Karma", sans-serif;
-  position: absolute;
-  left: 64.1667vw;
-  top: 135vh;
-}
-.race-against-time {
-  color: #ffffff;
-  text-align: left;
-  font: 600 2.0833vw "Karma", sans-serif;
-  position: absolute;
-  left: 70.9375vw;
-  top: 156.3vh;
-}
-.earn-league-points {
-  color: #ffffff;
-  text-align: left;
-  font: 600 2.0833vw "Karma", sans-serif;
-  position: absolute;
-  left: 76.71875vw;
-  top: 180vh;
-}
-#about_us {
-  background: linear-gradient(rgba(31,41,55,.5), rgba(31,41,55,.5)), url("../assets/team_bg.jpeg");;
-  background-repeat: no-repeat;
-  background-size: 100% auto;
-  background-position: center center;
+    color: #ffffff;
+    text-align: left;
+    font: 600 5vw "K2D", sans-serif;
+    position: absolute;
+    left: 3.45vw;
+    top: 130vh;
+    text-shadow: 0 0 7px black;
+  }
+  .an-alternate-reality-an-alternate-vision {
+    color: #ffffff;
+    text-align: left;
+    font: 600 1.667vw "K2D", sans-serif;
+    position: absolute;
+    left: 3.45vw;
+    top: 145vh;
+    text-shadow: 0 0 7px black;
+  }
+  .learn {
+    color: #d0f4ff;
+    text-align: left;
+    font: 600 4.1667vw "KoHo", sans-serif;
+    position: absolute;
+    left: 60.677vw;
+    top: 122.5vh;
+  }
+  .compete {
+    color: #d0f4ff;
+    text-align: left;
+    font: 600 4.1667vw "KoHo", sans-serif;
+    position: absolute;
+    left: 77.2916vw;
+    top: 143.88vh;
+  }
+  .climb {
+    color: #d0f4ff;
+    text-align: left;
+    font: 600 4.1667vw "KoHo", sans-serif;
+    position: absolute;
+    left: 69.0625vw;
+    top: 167.5vh;
+  }
+  .browse-extensive-course-material {
+    color: #ffffff;
+    text-align: left;
+    font: 600 2.0833vw "Karma", sans-serif;
+    position: absolute;
+    left: 64.1667vw;
+    top: 135vh;
+  }
+  .race-against-time {
+    color: #ffffff;
+    text-align: left;
+    font: 600 2.0833vw "Karma", sans-serif;
+    position: absolute;
+    left: 70.9375vw;
+    top: 156.3vh;
+  }
+  .earn-league-points {
+    color: #ffffff;
+    text-align: left;
+    font: 600 2.0833vw "Karma", sans-serif;
+    position: absolute;
+    left: 76.71875vw;
+    top: 180vh;
+  }
+  #about_us {
+    background: linear-gradient(rgba(31,41,55,.5), rgba(31,41,55,.5)), url("../assets/team_bg.jpeg");;
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+    background-position: center center;
+  }
 
-}
+  /* Media Queries for Responsiveness */
+  @media (max-width: 768px) {
+    .q-n-e-x-u-s {
+      font-size: 8vw;
+      left: 5vw;
+      top: 130vh;
+    }
+    .an-alternate-reality-an-alternate-vision {
+      font-size: 3vw;
+      left: 5vw;
+      top: 140vh;
+    }
+    .learn, .compete, .climb {
+      font-size: 6vw;
+      left: 10vw;
+      top: auto;
+    }
+    .browse-extensive-course-material, .race-against-time, .earn-league-points {
+      font-size: 4vw;
+      left: 10vw;
+      top: auto;
+    }
+    .ellipse-1 {
+      width: 50vw;
+      height: 50vw;
+      left: 25vw;
+      top: 100vh;
+    }
+    #discoverimg {
+      height: 50vh;
+    }
+    #discover, #about_us {
+      display: none;
+    }
+    .h-screen {
+      height: 100vh;
+    }
+    .text-8xl {
+      font-size: 4rem;
+    }
+    .text-4xl {
+      font-size: 2rem;
+    }
+    .btn {
+      font-size: 1rem;
+      padding: 0.5rem 1rem;
+    }
+  }
 </style>
 <div id="main" class="grid grid-rows-3">
   <div class="h-screen flex flex-col items-center justify-center gap-y-4 bg-fixed" style="background-image:url({bg1}); background-size: auto 100%;">
-      <div class="flex gap-5">
-      <img src={QnexusColor} alt=""/>
+      <div class="flex gap-5 items-center">
+        <img src={QnexusColor} alt="" class="w-20 h-20"/>
         <p style="font-family: Merriweather" class="text-8xl text-white">QNexus</p>
       </div>
       <div class="text-4xl font-bold font-sans">Game Hard, Learn Harder</div>
